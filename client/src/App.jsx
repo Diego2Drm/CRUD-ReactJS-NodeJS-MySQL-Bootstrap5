550323632d84488cde5fdc1c6e4900317e60da2a
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-08-07 19:15:37 -0600

    feat, style: Formulario

---
+++ client/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState('');
+  const [job, setJob] = useState('');
+  const [years, setYears] = useState(0);
+
+  const addData = () => {
+    
+  }
+
+  return (
+    <section className='App'>
+      <h1>CRUD React + Noded + MySql</h1>
+      <div className='datos'>
+        <label>
+          <input type="text" onChange={(e) => setName(e.target.value)} />
+          <span>Name:</span>
+        </label>
+
+        <label>
+          <input type="text" onChange={(e) => setAge(e.target.value)} />
+          <span>Age:</span>
+        </label>
+
+        <label>
+          <input type="text" onChange={(e) => setCountry(e.target.value)} />
+          <span>Country:</span>
+        </label>
+
+        <label>
+          <input type="text" onChange={(e) => setJob(e.target.value)} />
+          <span>Job:</span>
+        </label>
+
+        <label>
+          <input type="text" onChange={(e) => setYears(e.target.value)} />
+          <span>Years:</span>
+        </label>
+
+
+        <button onClick={addData}>Registrar</button>
+      </div>
+    </section>
+  )
+}
+
+export default App
