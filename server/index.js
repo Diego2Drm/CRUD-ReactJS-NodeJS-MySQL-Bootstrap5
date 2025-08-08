ab4eae37a7cb9445907c95aa3a64ac9da8e840f5
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-08-07 21:03:49 -0600

    feat(cors): Instalación de cors, Conexión a MySql y Guardando  información en la base de datos

--- server/index.js
+++ server/index.js
@@ -1,7 +1,43 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+// conexión a la base de datos
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "employees_crud",
+});
+
+// gaurdar
+app.post("/create", (req, res) => {
+  // datos obtenidos del formulario
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const job = req.body.job;
+  const years = req.body.years;
+
+  // consulta y envio de los datos --> ?,?,? indica que le voy a enviar informacion 
+  db.query('INSERT INTO employees(name,age,country,job,years) VALUES(?,?,?,?,?)', [name,age,country,job,years],
+    (err,result) => {
+      if(err){
+        console.log(err);
+      } else{
+        res.send("Empleado registrado con éxito!!");
+      }
+    }
+  )
+});
+
+
 
 app.listen(3001, () => {
   console.log("listen port 3001");
-  
+
 });
