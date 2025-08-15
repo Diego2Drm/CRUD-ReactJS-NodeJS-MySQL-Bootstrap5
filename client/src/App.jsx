901b47647a58501f35db745f942a1df5cf21039b
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-08-14 19:50:54 -0600

    feat(Clinet): functions editEmployee, updateData, cleanData

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,11 @@ import { EmployeesTable } from "./components/EmployeesTable";
 
 function App() {
   const [name, setName] = useState('');
-  const [age, setAge] = useState(0);
+  const [age, setAge] = useState();
   const [country, setCountry] = useState('');
   const [job, setJob] = useState('');
-  const [years, setYears] = useState(0);
+  const [years, setYears] = useState();
+  const [id, setId] = useState();
 
   const addData = () => {
     Axios.post("http://localhost:3001/create", {
@@ -20,6 +21,7 @@ function App() {
     }).then(() => {
       getData();
       alert("Empleado Registrado!!")
+      cleanData();
     });
   }
 
@@ -31,21 +33,63 @@ function App() {
     });
   };
 
+  const [edit, setedit] = useState(false);
+
+  const editEmployee = (val) => {
+    setedit(true);
+    setName(val.name);
+    setAge(val.age);
+    setCountry(val.country);
+    setJob(val.job);
+    setYears(val.years);
+    setId(val.id);
+  }
+
+  const updateData = () => {
+    Axios.put("http://localhost:3001/update", {
+      id: id,
+      name: name,
+      age: age,
+      country: country,
+      job: job,
+      years: years,
+    }).then(() => {
+      getData();
+      alert("Empleado Actualizado!!");
+      cleanData();
+    });
+  }
+
+  const cleanData = () => {
+    setedit(false)
+    setName("");
+    setAge("");
+    setCountry("");
+    setJob("");
+    setYears("");
+    setId("");
+  }
+
   getData();
   return (
     <section className='container p-3'>
       <h1>CRUD React + Noded + MySql</h1>
+
       <div className='card bg-success-subtle'>
+
         <div className="card-header text-center">
           <h2 className="text-danger">Gestion de empleados</h2>
         </div>
+
         <div className="card-body">
+
           <label className="input-group mb-3">
             <span className="input-group-text" id="basic-addon1">
               <i className="bi bi-person-fill"></i>
             </span>
             <input type="text" className="form-control" placeholder="Name" aria-label="Name" aria-describedby="basic-addon1"
               onChange={(e) => setName(e.target.value)}
+              value={name}
             />
           </label>
 
@@ -55,6 +99,7 @@ function App() {
             </span>
             <input type="text" className="form-control" placeholder="Age" aria-label="Age" aria-describedby="basic-addon1"
               onChange={(e) => setAge(e.target.value)}
+              value={age}
             />
           </label>
 
@@ -64,6 +109,7 @@ function App() {
             </span>
             <input type="text" className="form-control" placeholder="Country" aria-label="Country" aria-describedby="basic-addon1"
               onChange={(e) => setCountry(e.target.value)}
+              value={country}
             />
           </label>
 
@@ -73,6 +119,7 @@ function App() {
             </span>
             <input type="text" className="form-control" placeholder="job" aria-label="Job" aria-describedby="basic-addon1"
               onChange={(e) => setJob(e.target.value)}
+              value={job}
             />
           </label>
 
@@ -82,18 +129,36 @@ function App() {
             </span>
             <input type="text" className="form-control" placeholder="Experience" aria-label="Experience" aria-describedby="basic-addon1"
               onChange={(e) => setYears(e.target.value)}
+              value={years}
             />
           </label>
+
         </div>
 
         <div className="card-footer text-center">
-          <button onClick={addData} className="btn btn-success">
-            Registrar
-          </button>
+          {
+            edit ?
+              <div className='d-flex justify-content-center gap-2'>
+                <button onClick={updateData} className="btn btn-outline-success">
+                  Update
+                </button>
+                <button onClick={cleanData} className="btn  btn-outline-danger">
+                  Cancel
+                </button>
+
+
+              </div>
+              :
+              <button onClick={addData} className="btn btn-success">
+                Register
+              </button>
+          }
+
         </div>
+
       </div>
 
-      <EmployeesTable  employeesLits={employeesLits}/>
+      <EmployeesTable employeesLits={employeesLits} handleEdit={editEmployee} />
 
     </section>
   )
--- client/src/components/EmployeesTable/index.jsx
+++ client/src/components/EmployeesTable/index.jsx
@@ -1,5 +1,5 @@
 
-const EmployeesTable = ({ employeesLits }) => {
+const EmployeesTable = ({ employeesLits, handleEdit }) => {
   return (
     <div className="">
       <table className="table table-striped-columns mt-5">
@@ -11,6 +11,7 @@ const EmployeesTable = ({ employeesLits }) => {
             <th scope="col">Country</th>
             <th scope="col">Job</th>
             <th scope="col">Experience</th>
+            <th scope="col">Actions</th>
           </tr>
         </thead>
         {
@@ -23,6 +24,14 @@ const EmployeesTable = ({ employeesLits }) => {
                 <td>{employe.country}</td>
                 <td>{employe.job}</td>
                 <td>{employe.years}</td>
+                <td className="btn-group" role="group">
+                  <button type="button" className="btn btn-info"
+                    onClick={() => handleEdit(employe)}
+                  >
+                    Edit
+                  </button>
+                  <button type="button" className="btn btn-danger">Delete</button>
+                </td>
               </tr>
             </tbody>
           ))
