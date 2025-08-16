d55af8451e70f926a2aaf54c27e0092105330f17
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-08-15 20:59:39 -0600

    feat(server): Add Method DELETE

--- server/index.js
+++ server/index.js
@@ -67,6 +67,20 @@ app.put("/update", (req, res) => {
     }
   )
 });
+// Borrar Datos
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id
+
+  db.query('DELETE FROM employees WHERE id=?', id,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  )
+});
 
 app.listen(3001, () => {
   console.log("listen port 3001");
